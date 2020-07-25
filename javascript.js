d492aba5824e634c9197e89d2136ed553eed7218
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-07-25 14:33:59 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var time = new Date();
+
+var d = time.getHours();
+var greeting="";
+var msg="";
+var msg1="";
+
+
+
+
+
+
+ if (d > 18)  {
+                msg = 'Open till 9pm';
+}
+
+else if (d > 12) {
+                   // greeting = document.getElementById('b').style.display= 'block'; // Delete Comment(two forward slash before greeting) Display Image & Text.
+                    msg1 ='Voucher Available';
+}
+
+else if ((d > 0) && (d < 7)) {
+      greeting = document.getElementById('c').style.display= 'block';}
+
+else if (d > 6) { greeting = document.getElementById('d').style.display= 'block';}
+
+else { greeting=""; }
+
+document.write('<p id="e">' + msg + '</p>');
+document.write('<p id="f">' + msg1 + '</p>');
+document.image = greeting;
